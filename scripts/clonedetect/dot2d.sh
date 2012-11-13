a385984c2c276d640cf69a2eb40f141af72cddae
skyhover <skyhover.pi@gmail.com>
2012-11-13 09:39:01 +0800

    scripts enhancements to be more error-resillient, and provide more debugging info for deckard 2.x

--- scripts/clonedetect/dot2d.sh
+++ scripts/clonedetect/dot2d.sh
@@ -52,11 +52,12 @@ case $SRCLANG in
 		exit 1 ;;
 esac
 
-echo -n "Vector generation..."
+echo -n "== Vector generation..."
+echo 
 "`dirname $0`/vdbgendot" $TOOVERWRITE
 errcode=$?
 if [[ $errcode -ne 0 ]]; then
-	echo "Error: problem in vec generator step. Stop and check logs in $TIME_DIR/"
+	echo "Error: problem in vec generator step. Likely no vector generated. Stop and check logs in $TIME_DIR/vgen_*"
 	exit $errcode
 else
 	echo "Vector generation done. Logs in $TIME_DIR/vgen_*"
@@ -64,7 +65,8 @@ else
 	echo
 fi
 
-echo "Vector clustering and filtering..."
+echo "== Vector clustering and filtering..."
+echo 
 "`dirname $0`/vertical-param-batch" $TOOVERWRITE
 errcode=$?
 if [[ $errcode -ne 0 ]]; then
@@ -72,19 +74,21 @@ if [[ $errcode -ne 0 ]]; then
 	exit $errcode
 fi
 
+echo
 echo "Clone detection done. Logs in $TIME_DIR/*"
 echo "Clone reports in $CLUSTER_DIR/post_cluster_*"
 echo
 
-# Bug Finding:
-echo "In addition, potential clone-related bugs may be produced by running the scripts:"
-echo "(Be careful about the file overwriting and the choice for programming language)"
-
-echo "(1) search clone reports and find out suspicious ones: "
-echo "    \"$DECKARD_DIR/scripts/bugdetect/bugfiltering\" \"${CLUSTER_DIR}/post_<filename>\" $SRCLANG > \"${CLUSTER_DIR}/bug_<filename>\" 2> \"${TIME_DIR}/bugfiltering_<filename>\""
-echo "(2) transform the bug reports to html for easier investigation:"
-echo "    \"$DECKARD_DIR/src/main/out2html\" \"${CLUSTER_DIR}/bug_<filename>\" > \"${CLUSTER_DIR}/bug_<filename>.html\""
-echo
+# Bug Finding (not working for dot2d.sh yet):
+## echo "In addition, potential clone-related bugs may be produced by running the scripts:"
+## echo "(Be careful about the file overwriting and the choice for programming language)"
+## 
+## echo "(1) search clone reports and find out suspicious ones: "
+## echo "    \"$DECKARD_DIR/scripts/bugdetect/bugfiltering\" \"${CLUSTER_DIR}/post_<filename>\" $SRCLANG > \"${CLUSTER_DIR}/bug_<filename>\" 2> \"${TIME_DIR}/bugfiltering_<filename>\""
+## echo "(2) transform the bug reports to html for easier investigation:"
+## echo "    \"$DECKARD_DIR/src/main/out2html\" \"${CLUSTER_DIR}/bug_<filename>\" > \"${CLUSTER_DIR}/bug_<filename>.html\""
+## echo
+## 
 ## # the actual commands for bug finding:
 ## find "$CLUSTER_DIR" -type f -name "post_cluster_vdb_*_*_allg_*_*" | while read cdb;
 ## do
