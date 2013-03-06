e341358e3f2d10cd7311a0d33f0be17a89e22ff0
Lingxiao Jiang <jianglingxiao@gmail.com>
2013-03-06 17:06:28 +0800

    update packaging scripts

---
+++ Notes/packaging.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "TODO scripts:"
+
+echo "1. check out an appropriate version of the repo"
+echo "2. add license info"
+echo "3. copy appropriate files into the repo for github"
+echo "4. push to github"
+echo "5. zip appropriate files for archive"
+
+exit 1
+
